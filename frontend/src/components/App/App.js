f087effddaa9d29a3006f99de65252babc15bed1
fleather2 <fleather2@users.noreply.github.com>
2024-10-23 08:44:36 -0400

    Adding components

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
